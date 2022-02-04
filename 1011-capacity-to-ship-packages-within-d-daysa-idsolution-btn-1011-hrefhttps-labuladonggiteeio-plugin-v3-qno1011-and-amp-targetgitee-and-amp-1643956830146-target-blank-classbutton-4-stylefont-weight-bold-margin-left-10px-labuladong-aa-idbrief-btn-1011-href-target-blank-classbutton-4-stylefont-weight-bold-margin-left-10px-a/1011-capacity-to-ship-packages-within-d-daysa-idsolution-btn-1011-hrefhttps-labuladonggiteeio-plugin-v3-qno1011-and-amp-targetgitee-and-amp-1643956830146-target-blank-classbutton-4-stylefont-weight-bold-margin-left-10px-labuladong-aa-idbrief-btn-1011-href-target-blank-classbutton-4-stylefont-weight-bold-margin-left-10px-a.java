a805db91f04c1b988e8cc1861c3ebcb127b91c06
DLCoder <lindui1994@gmail.com>
2022-02-03 22:51:01 -0800

    Time: 9 ms (75.89%), Space: 45.8 MB (60.79%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-daysa-idsolution-btn-1011-hrefhttps-labuladonggiteeio-plugin-v3-qno1011-and-amp-targetgitee-and-amp-1643956830146-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-1011-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/1011-capacity-to-ship-packages-within-d-daysa-idsolution-btn-1011-hrefhttps-labuladonggiteeio-plugin-v3-qno1011-and-amp-targetgitee-and-amp-1643956830146-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-1011-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
+++ 1011-capacity-to-ship-packages-within-d-daysa-idsolution-btn-1011-hrefhttps-labuladonggiteeio-plugin-v3-qno1011-and-amp-targetgitee-and-amp-1643956830146-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-1011-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/1011-capacity-to-ship-packages-within-d-daysa-idsolution-btn-1011-hrefhttps-labuladonggiteeio-plugin-v3-qno1011-and-amp-targetgitee-and-amp-1643956830146-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-1011-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -2,6 +2,7 @@ class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int left = 0, right = 0;
         for (int i : weights) {
+            // most important part: left bound >= max of weight array! right bound <= sum
             left = Math.max(i, left);
             right += i;
         }
@@ -21,7 +22,8 @@ class Solution {
         for (int i : weights) {
             if (eachShipLoadSum + i > capacity) {
                 days -= 1;
-                if (days <= 0) return false;
+                if (days == 0) return false;
+                // the load for that day would be moved to next day so eachShipLoadSum = i;
                 eachShipLoadSum = i;
             } else {
                 eachShipLoadSum += i;
