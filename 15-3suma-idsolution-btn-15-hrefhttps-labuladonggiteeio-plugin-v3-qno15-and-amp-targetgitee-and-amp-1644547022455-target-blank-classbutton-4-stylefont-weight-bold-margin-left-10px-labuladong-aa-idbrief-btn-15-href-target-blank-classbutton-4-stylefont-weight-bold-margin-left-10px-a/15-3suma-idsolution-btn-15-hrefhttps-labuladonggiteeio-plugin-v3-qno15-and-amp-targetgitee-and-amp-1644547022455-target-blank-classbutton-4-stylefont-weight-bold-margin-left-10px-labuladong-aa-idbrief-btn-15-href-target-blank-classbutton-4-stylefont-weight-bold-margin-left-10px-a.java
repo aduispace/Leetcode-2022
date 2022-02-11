95071b31b5abdf8849a2def33d15e2b54a305539
DLCoder <lindui1994@gmail.com>
2022-02-10 23:30:40 -0800

    Time: 43 ms (39.91%), Space: 61.9 MB (19.80%) - LeetHub

--- 15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644547022455-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644547022455-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
+++ 15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644547022455-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644547022455-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -1,11 +1,16 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        // total complexity: O(NlogN + N^2)
+        // sort: O(NlogN)
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
+        // for + twoSum while loop : O(N^2)
         for (int i = 0; i < nums.length - 2; i++) {
             int target = 0 - nums[i];
             List<List<Integer>> res = twoSum(nums, target, i + 1);
             result.addAll(res);
+            
+            // need this to deduplicate the first candidate!!!
             while (i < nums.length - 2 && nums[i] == nums[i+1]) {
                 i++;
             }
@@ -17,11 +22,15 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         int left = start, right = nums.length - 1;
         
+        
+        // two pointer time: O(N)
         while (left < right) {
             int sum = nums[left] + nums[right];
             if (sum == target) {
                 int a = nums[left], b = nums[right];
                 res.add(new ArrayList<Integer>(Arrays.asList(nums[start - 1], a, b)));
+                
+                // deduplicate candidate
                 while (nums[left] == a && left < nums.length - 1) {
                     left++;
                 }
@@ -30,11 +39,14 @@ class Solution {
                 }
             } else if (sum > target) {
                 int b = nums[right];
+                
+                // deduplicate candidate
                 while (nums[right] == b && right > 0) {
                     right--;
                 }
             } else {
                 int a = nums[left];
+                // deduplicate candidate
                 while (nums[left] == a && left < nums.length - 1) {
                     left++;
                 }
