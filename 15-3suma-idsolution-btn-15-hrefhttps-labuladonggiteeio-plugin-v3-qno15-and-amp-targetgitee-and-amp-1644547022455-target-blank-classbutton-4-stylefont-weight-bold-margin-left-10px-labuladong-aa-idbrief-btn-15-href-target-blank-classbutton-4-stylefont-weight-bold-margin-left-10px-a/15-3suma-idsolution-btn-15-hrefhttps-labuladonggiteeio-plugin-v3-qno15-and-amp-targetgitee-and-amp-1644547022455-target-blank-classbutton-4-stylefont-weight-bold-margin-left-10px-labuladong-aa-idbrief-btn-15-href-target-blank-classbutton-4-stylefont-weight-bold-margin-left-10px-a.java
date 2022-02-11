7b3ca8a3d80069b793f4701634d09aca45028be3
DLCoder <lindui1994@gmail.com>
2022-02-10 23:09:59 -0800

    Time: 44 ms (38.83%), Space: 62.5 MB (19.62%) - LeetHub

---
+++ 15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644547022455-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644547022455-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -0,0 +1,74 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int target = 0 - nums[i];
+            List<List<Integer>> res = twoSum(nums, target, i + 1);
+            result.addAll(res);
+            while (i < nums.length - 2 && nums[i] == nums[i+1]) {
+                i++;
+            }
+        }
+        return result;
+    }
+    
+    private List<List<Integer>> twoSum(int[] nums, int target, int start) {
+        List<List<Integer>> res = new ArrayList<>();
+        int left = start, right = nums.length - 1;
+        
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                int a = nums[left], b = nums[right];
+                res.add(new ArrayList<Integer>(Arrays.asList(nums[start - 1], a, b)));
+                while (nums[left] == a && left < nums.length - 1) {
+                    left++;
+                }
+                while (nums[right] == b && right > 0) {
+                    right--;
+                }
+            } else if (sum > target) {
+                int b = nums[right];
+                while (nums[right] == b && right > 0) {
+                    right--;
+                }
+            } else {
+                int a = nums[left];
+                while (nums[left] == a && left < nums.length - 1) {
+                    left++;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         if (nums.length < 3) {
+//             return result;
+//         }
+        
+//         HashSet<List<Integer>> deduplicatedResult = new HashSet<>();
+              
+//         for (int i = 0; i < nums.length - 2; i++) {
+//             for (int j = i + 1; j < nums.length - 1; j++) {
+//                 for (int m = j + 1; m < nums.length; m++) {
+//                     int sum = nums[i] + nums[j] + nums[m];
+//                     if (sum == 0) {
+//                         ArrayList<Integer> cur = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[m]));
+//                         cur.sort(Comparator.naturalOrder());
+//                         if (!deduplicatedResult.contains(cur)) {
+//                             result.add(cur);
+//                             deduplicatedResult.add(cur);
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return result;
+//     }
+// }
