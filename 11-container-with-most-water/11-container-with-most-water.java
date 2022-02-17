eca7cadc59cf433c034f46e60d0dd8ad6c0c3790
DLCoder <lindui1994@gmail.com>
2022-02-17 01:22:03 -0800

    Time: 3 ms (91.89%), Space: 73.9 MB (60.09%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -4,6 +4,8 @@ class Solution {
         int left = 0, right = height.length - 1;
         while (left < right) {
             int amount = getWaterAmount(left, right, height);
+            // update maxArea before move pointer
+            // 核心在于：每次移动相对高度即height[i]小的那一边
             if (maxArea < amount) maxArea = amount;
             if (height[left] > height[right]) {
                 right--;
