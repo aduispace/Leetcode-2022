c41536d055b7ddd70d53705fab2844c925a57232
DLCoder <lindui1994@gmail.com>
2022-02-17 21:39:52 -0800

    Time: 1 ms (92.60%), Space: 46.4 MB (10.46%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int trap(int[] height) {
+        // 每个位置i存水的量为 min(leftMax[i], rightMax[i]) - height[i]
+        int trap = 0;
+        if (height.length < 3) {
+            return trap;
+        }
+        int leftMax = height[0], rightMax = height[height.length - 1]; // 边界线不能存水
+        int left = 0, right = height.length - 1;
+        while (left <= right) {
+            if (leftMax < rightMax) {
+                // min(leftMax, rightMax), in this case it is leftMax
+                // 先update leftMax！！！确定当前left高度如果比leftmax高，此处不存水
+                leftMax = Math.max(height[left], leftMax);
+                trap += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(height[right], rightMax);
+                trap += rightMax - height[right];
+                right--;
+            }
+        }
+        
+        return trap;
+        // return trap + Math.min(leftMax, rightMax) - height[left];
+    }
+}
