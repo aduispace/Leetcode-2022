293f0f4ff560add9eadc3e4dc2f58235177266a1
DLCoder <lindui1994@gmail.com>
2022-02-17 22:20:58 -0800

    Time: 11 ms (42.67%), Space: 54 MB (12.57%) - LeetHub

---
+++ 455-assign-cookies/455-assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int contentChildren = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int leftG = 0, leftS = 0;
+        while (leftG <= g.length - 1 && leftS <= s.length - 1) {
+            if (g[leftG] <= s[leftS]) {
+                contentChildren++;
+                leftG++;
+                leftS++;
+            } else {
+                leftS++;
+            }
+        }
+        
+        return contentChildren;
+    }
+}
