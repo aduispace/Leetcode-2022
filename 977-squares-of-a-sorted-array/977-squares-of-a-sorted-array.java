f1ad2c725252cc2582f8999b539b9b5bf7ae2f10
DLCoder <lindui1994@gmail.com>
2022-02-23 00:19:51 -0800

    Time: 2 ms (80.07%), Space: 55.1 MB (31.66%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -3,6 +3,8 @@ class Solution {
         int[] result = new int[nums.length];
         int n = nums.length;
         int left = 0, right = nums.length - 1;
+        
+        // 双指针: [-4,-1,0,3,10] 左右开始往中间都是一个递减序列，所以result从后往前assign
         while (n > 0) {
             if (Math.abs(nums[left]) <= Math.abs(nums[right])) {
                 result[--n] = nums[right] * nums[right];
