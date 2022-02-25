ff8d5ead2b7fcecf820310023c8fc0bb5152cea7
DLCoder <lindui1994@gmail.com>
2022-02-24 22:11:30 -0800

    Time: 1 ms (88.90%), Space: 47.4 MB (33.27%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -2,7 +2,6 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         // fast find first element that different with slow 
         // edge case [0, 1, 2, 3, 4, 5]
-        // regular case [0, 1, 2, 3, 4, 3, 4]
         int fast = 0, slow = 0;
         while (fast < nums.length) {
             if (nums[fast] != nums[slow]) {
@@ -11,6 +10,7 @@ class Solution {
             }
             fast++;
         }
+        // slow would stop at the end index of first part of array, need to +1 to get its length
         return slow + 1;
     }
 }
