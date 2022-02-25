f9d6d6540f38d12c02b7b77043e5bd2b3448a3c7
DLCoder <lindui1994@gmail.com>
2022-02-24 21:20:02 -0800

    Time: 2 ms (73.38%), Space: 55.3 MB (6.61%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // slow to assign non-zero element, fast to find non-zero element
+        int slow = 0, fast = 0;
+        while (slow < nums.length) {
+            if (fast < nums.length && nums[fast] != 0) {
+                nums[slow] = nums[fast];
+                slow++;
+                fast++;
+            } else if (fast == nums.length) {
+                nums[slow] = 0;
+                slow++;
+            } else {
+                fast++;
+            }
+        }
+    }
+}
