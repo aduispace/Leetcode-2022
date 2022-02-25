65936fda12caf49d190863e154725bd59cba09fe
DLCoder <lindui1994@gmail.com>
2022-02-24 21:21:59 -0800

    Time: 2 ms (73.38%), Space: 54.5 MB (27.98%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,6 +1,7 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         // slow to assign non-zero element, fast to find non-zero element
+        // the exit condition is slow reach the end of array
         int slow = 0, fast = 0;
         while (slow < nums.length) {
             if (fast < nums.length && nums[fast] != 0) {
