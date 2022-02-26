f9ccb3cef996ddcd81eb1f3e3b7dd5ab1cd34a29
DLCoder <lindui1994@gmail.com>
2022-02-25 23:00:42 -0800

    Time: 11 ms (70.48%), Space: 43.9 MB (34.22%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
@@ -11,7 +11,7 @@ class Solution {
             if (rec.getOrDefault(chRight, 0) == 1) {
                 distinct++;
             }
-            // in any case we could move right border, just move left to shrink the distinct
+            // in any case we could move right border, just move left to shrink the distinct: `if (distinct <= k)` condition is useless in this case because it would go through move left of window to keep distinct at most K
             right++;
             while (left < s.length() && distinct > k) {
                 char chLeft = s.charAt(left);
