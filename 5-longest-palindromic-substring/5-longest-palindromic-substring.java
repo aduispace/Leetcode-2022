172ef647c2ca1288baa907ce15ba3424a40f987f
DLCoder <lindui1994@gmail.com>
2022-03-01 00:01:35 -0800

    Time: 84 ms (42.28%), Space: 54 MB (31.96%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,7 +1,9 @@
 class Solution {
     public String longestPalindrome(String s) {
         String result = "";
+        // Time Complexity: O(N^2)
         for (int i = 0; i < s.length(); i++) {
+            // 单数子串和双数子串
             String odd = palindrome(s, i, i);
             String even = palindrome(s, i, i + 1);
             String curResult = odd.length() > even.length() ? odd : even;
@@ -13,12 +15,14 @@ class Solution {
     
     private String palindrome(String s, int left, int right) {
         if (s.length() <= 1) return s;
+        // 注意start spanning condition 
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             // span from middle, left go left, right go right
             left--;
             right++;
         }
         
+        // left, right invalid; left + 1, right - 1 valid
         return s.substring(left + 1, right);
     }
 }
