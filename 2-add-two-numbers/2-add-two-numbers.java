688b0dfd15b56efdf2bd304193404669ec549d51
DLCoder <lindui1994@gmail.com>
2022-03-01 21:43:27 -0800

    Time: 3 ms (73.79%), Space: 47.9 MB (26.24%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -20,26 +20,35 @@ class Solution {
         ListNode dummyHead = new ListNode();
         ListNode cur = dummyHead;
         while (p1 != null || p2 != null) {
-            if (p1 != null && p2 != null) {
-                int val = (p1.val + p2.val + carryOver) % 10;
-                cur.next = new ListNode(val);
-                carryOver = (p1.val + p2.val + carryOver) / 10;
-                p1 = p1.next;
-                p2 = p2.next;
-                cur = cur.next;
-            } else if (p1 == null && p2 != null) {
-                int val = (p2.val + carryOver) % 10;
-                cur.next = new ListNode(val);
-                carryOver = (p2.val + carryOver) / 10;
-                p2 = p2.next;
-                cur = cur.next;
-            } else if (p1 != null && p2 == null) {
-                int val = (p1.val + carryOver) % 10;
-                cur.next = new ListNode(val);
-                carryOver = (p1.val + carryOver) / 10;
-                p1 = p1.next;
-                cur = cur.next;
-            }
+            int val1 = p1 == null ? 0 : p1.val;
+            int val2 = p2 == null ? 0 : p2.val;
+            int val = (val1 + val2 + carryOver) % 10;
+            cur.next = new ListNode(val);
+            carryOver = (val1 + val2 + carryOver) / 10;
+            if (p1 != null) p1 = p1.next;
+            if (p2 != null) p2 = p2.next;
+            cur = cur.next;
+            
+            // if (p1 != null && p2 != null) {
+            //     int val = (p1.val + p2.val + carryOver) % 10;
+            //     cur.next = new ListNode(val);
+            //     carryOver = (p1.val + p2.val + carryOver) / 10;
+            //     p1 = p1.next;
+            //     p2 = p2.next;
+            //     cur = cur.next;
+            // } else if (p1 == null && p2 != null) {
+            //     int val = (p2.val + carryOver) % 10;
+            //     cur.next = new ListNode(val);
+            //     carryOver = (p2.val + carryOver) / 10;
+            //     p2 = p2.next;
+            //     cur = cur.next;
+            // } else if (p1 != null && p2 == null) {
+            //     int val = (p1.val + carryOver) % 10;
+            //     cur.next = new ListNode(val);
+            //     carryOver = (p1.val + carryOver) / 10;
+            //     p1 = p1.next;
+            //     cur = cur.next;
+            // }
         }
         
         if (carryOver != 0) {
