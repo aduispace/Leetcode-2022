acd15d3b0dda04d28c2f2d3304e165f643dd0287
DLCoder <lindui1994@gmail.com>
2022-03-06 20:31:58 -0800

    Time: 12 ms (84.79%), Space: 55.6 MB (8.50%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -3,6 +3,16 @@ class Solution {
         // [1, 3, 5, 6, 7, 10, 2, 4] [1, 3, 5, 6, 7, 10, 2, 4]
         // [3, 5, 6, 7, 10, 4, 5]
         // [1 3 5 6 7 10]
+        // follow up question of next greater element: 
+        // 先把nums里面数字用stack处理一遍，也可以取模one pass: 
+        
+        // for (int i = 2 * nums.length - 1; i >= 0; --i) {
+        //     while (!stack.empty() && nums[stack.peek()] <= nums[i % nums.length]) {
+        //         stack.pop();
+        //     }
+        //     res[i % nums.length] = stack.empty() ? -1 : nums[stack.peek()];
+        //     stack.push(i % nums.length);
+        // }
         
         Deque<Integer> stack = new ArrayDeque<>();
         int[] result = new int[nums.length];
