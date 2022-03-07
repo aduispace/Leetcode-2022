8c1049a68d1fd527a99c99cd63a4eb8b0f8812bf
DLCoder <lindui1994@gmail.com>
2022-03-06 16:54:17 -0800

    Time: 38 ms (75.24%), Space: 133.3 MB (19.42%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -3,7 +3,7 @@ class Solution {
         int len = temperatures.length;
         int[] result = new int[len];
         // stack definition: monotone stack that all elements on the right side greater than current temperatures[i]
-        // 
+        // pop first, then determine the current result value (distance) based on left elements in stack
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i = len - 1; i >= 0; i--) {
             while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
