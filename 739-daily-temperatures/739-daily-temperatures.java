2a687ab967dc14ddcd9c00066b8880854dd9a448
DLCoder <lindui1994@gmail.com>
2022-03-06 16:52:51 -0800

    Time: 32 ms (81.29%), Space: 130.4 MB (30.55%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        int[] result = new int[len];
+        // stack definition: monotone stack that all elements on the right side greater than current temperatures[i]
+        // 
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = len - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                result[i] = 0;
+            } else {
+                result[i] = stack.peek() - i;
+            }
+            // 最后再把当前元素push进去，之前都是当前元素和它右边元素的比较
+            stack.push(i);
+        }
+        
+        return result;
+    }
+}
