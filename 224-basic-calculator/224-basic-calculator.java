0b4d7430a3b48649a46ea140d054214b48a234ce
DLCoder <lindui1994@gmail.com>
2022-03-09 15:50:41 -0800

    Time: 4 ms (97.54%), Space: 44.4 MB (43.02%) - LeetHub

---
+++ 224-basic-calculator/224-basic-calculator.java
@@ -0,0 +1,39 @@
+class Solution {
+    int i = 0;
+    public int calculate(String s) {
+
+        // 1 + 1 -> +1 +1
+        // 
+        return helper("(" + s + ")");
+    }
+    
+    private int helper(String s) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int sign = 1;
+        int res = 0;
+        int num = 0;
+        while (i < s.length()) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+                i++;
+            } else if (ch == '+' || ch == '-') {
+                res += sign * num;
+                sign = ch == '+' ? 1 : -1;
+                num = 0;
+                i++;
+            } else if (ch == '(') {
+                i++;
+                res += sign * helper(s);
+            } else if (ch == ')') {
+                res += sign * num;
+                i++;
+                return res;
+            } else {
+                i++;
+            }
+        }
+        
+        return res;
+    }
+}
