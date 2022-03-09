91ebdf00cd8b11a2ddb4ceafbcd78488c9c11ce8
DLCoder <lindui1994@gmail.com>
2022-03-08 17:46:39 -0800

    Time: 6 ms (79.62%), Space: 44.2 MB (35.07%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        // number一定比operator多一个
+        Deque<Integer> numberStack = new ArrayDeque<>();
+        int result = 0;
+        for (String i : tokens) {
+            if (isOperator(i)) {
+                int a = numberStack.pop();
+                int b = numberStack.pop();
+                result = calculate(a, b, i);
+                numberStack.push(result);
+            } else {
+                numberStack.push(Integer.parseInt(i));
+            }
+        }
+        
+        return numberStack.pop();
+    }
+    
+    private boolean isOperator (String str) {
+        if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")) {
+            return true;
+        }
+        return false;
+    }
+    
+    private int calculate(int a, int b, String str) {
+        if (str.equals("+")) {
+            return a + b;
+        } else if (str.equals("-")){
+            return b - a;
+        } else if (str.equals("*")) {
+            return a * b;
+        } else {
+            return b / a;
+        }
+    }
+}
