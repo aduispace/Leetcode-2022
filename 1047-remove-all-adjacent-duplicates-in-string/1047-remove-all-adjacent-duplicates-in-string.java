a41a3fb23c8a278851d567b4797ee2e2fb858abe
DLCoder <lindui1994@gmail.com>
2022-03-10 01:36:40 -0800

    Time: 43 ms (53.15%), Space: 48 MB (68.73%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -4,6 +4,8 @@ class Solution {
         
         for (int i = 0; i < sb.length(); i++) {
             if (i > 0 && sb.charAt(i) == sb.charAt(i - 1)) {
+                // stringBuilder delete end here won't overflow, checkout https://docs.oracle.com/javase/7/docs/api/java/lang/StringBuilder.html#delete(int,%20int)
+                // 用delete无需check end是否超出界限
                 sb.delete(i - 1, i + 1);
                 i = i - 2;
             }
