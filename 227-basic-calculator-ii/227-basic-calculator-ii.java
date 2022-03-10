2ed8a9a85aaa3b23a1a5a5e6e2295585d6f935d2
DLCoder <lindui1994@gmail.com>
2022-03-09 23:03:42 -0800

    Time: 10 ms (83.08%), Space: 42.2 MB (74.68%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int calculate(String s) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int num = 0;
+        char sign = '+';
+        int i = 0;
+        while (i < s.length()) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+            
+            if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
+                switch (sign) {
+                    case '+': stack.push(num);
+                    break;
+                    case '-': stack.push(0 - num);
+                    break;
+                    case '*': stack.push(stack.pop() * num);
+                    break;
+                    case '/': stack.push(stack.pop() / num);
+                    break;
+                }
+                sign = ch;
+                num = 0;
+            }
+            i++;
+        }
+              
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        
+        return result;
+    }
+}
