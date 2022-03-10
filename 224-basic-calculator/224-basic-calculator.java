f325f6da5531dce9480542c09f3597d5c55973d7
DLCoder <lindui1994@gmail.com>
2022-03-09 22:23:46 -0800

    Time: 8 ms (81.13%), Space: 44.3 MB (47.13%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.java
+++ 224-basic-calculator/224-basic-calculator.java
@@ -1,74 +1,38 @@
 class Solution {
-    int i = 0;
-    public int calculate(String s) {
-
-        // 1 + 1 -> +1 +1
-        // recursion: find "(" and start recursion, find ")" stop recursion and result
-        return helper(s);
-    }
-    
-    private int helper(String s) {
-        int sign = 1;
-        int res = 0;
-        int num = 0;
-        while (i < s.length()) {
-            char ch = s.charAt(i);
-            if (Character.isDigit(ch)) {
-                // how to get num char by char
-                num = num * 10 + (ch - '0');
-                i++;
-            } else if (ch == '+' || ch == '-') {
-                res += sign * num;
-                sign = ch == '+' ? 1 : -1;
-                num = 0;
-                i++;
-            } else if (ch == '(') {
-                i++;
-                res += sign * helper(s);
-            } else if (ch == ')') {
-                res += sign * num;
-                i++;
-                return res;
-            } else {
-                i++;
-            }
+public int calculate(String s) {
+    Stack<Integer> stack = new Stack<Integer>();
+    int result = 0;
+    int number = 0;
+    int sign = 1;
+    for(int i = 0; i < s.length(); i++){
+        char c = s.charAt(i);
+        if(Character.isDigit(c)){
+            number = 10 * number + (int)(c - '0');
+        }else if(c == '+'){
+            result += sign * number;
+            number = 0;
+            sign = 1;
+        }else if(c == '-'){
+            result += sign * number;
+            number = 0;
+            sign = -1;
+        }else if(c == '('){
+            //we push the result first, then sign;
+            stack.push(result);
+            stack.push(sign);
+            //reset the sign and result for the value in the parenthesis
+            sign = 1;   
+            result = 0;
+        }else if(c == ')'){
+            result += sign * number;  
+            number = 0;
+            sign = 1;
+            result *= stack.pop();    //stack.pop() is the sign before the parenthesis
+            result += stack.pop();   //stack.pop() now is the result calculated before the parenthesis
+            
         }
-        
-        // 最后num得被清0，没被清0的要加上
-        return res + num * sign;
- }    
-    
-//     int i;
-
-//     public int calculate(String s) {
-
-//         int operand = 0;
-//         int result = 0; // For the on-going result
-//         int nextOperandSign = 1;  // 1 means positive, -1 means negative
-
-//         while(i < s.length()) {
-
-//             char ch = s.charAt(i++);
-
-//             if (ch == ' ' || Character.isDigit(ch)) {
-//                 operand = (ch == ' ') ? operand : 10 * operand + (ch - '0');
-                
-//             } else if (ch == '(') {
-//                 operand = calculate(s);
-                
-//             } else if (ch == ')') {
-//                 // call exit也行
-//                 return result + nextOperandSign * operand; // Sub-expression we were evaluating has ended. Exit now...
-                
-//             } else { 
-//                 // If we're here, we just read the operator for the next operand/expression.
-//                 // Evaluate the existing expression already read, reset operand, set sign for next incoming operand.
-//                 result += nextOperandSign * operand;
-//                 nextOperandSign = ch == '+' ? 1 : -1;
-//                 operand = 0;
-//             }
-//         }
-
-//         return result + (nextOperandSign * operand);
-//     }
+    }
+    if(number != 0) result += sign * number;
+    return result;
+}
 }
