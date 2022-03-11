b47fdd3d8f2130347be9d3fc344b4b990500158e
DLCoder <lindui1994@gmail.com>
2022-03-10 16:20:24 -0800

    Time: 11 ms (96.92%), Space: 42.8 MB (86.78%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        // stack for left parentheses position
+        StringBuilder sb = new StringBuilder(s);
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i < sb.length(); i++) {
+            char ch = sb.charAt(i);
+            if (ch == '(') {
+                stack.push(i);
+            } else if (ch == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    sb.deleteCharAt(i);
+                    i = i - 1;
+                }
+            }
+        }
+        
+        while (!stack.isEmpty()) {
+            sb.deleteCharAt(stack.pop());
+        }
+        
+        return sb.toString();
+    }
+}
