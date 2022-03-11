eb6f1e08979c32dff6d7bb959e05c413c70ef12f
DLCoder <lindui1994@gmail.com>
2022-03-11 01:16:05 -0800

    Time: 0 ms (100.00%), Space: 41.8 MB (37.22%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -7,13 +7,12 @@ class MyStack {
         
     }
     
+    // O(1)
     public void push(int x) {
-        // while (!q2.isEmpty()) {
-        //     q1.offer(q2.poll());
-        // }
         q1.offer(x);
     }
     
+    // O(2N)
     public int pop() {
         while(!q1.isEmpty() && q1.size() > 1) {
             q2.offer(q1.poll());
@@ -26,12 +25,14 @@ class MyStack {
         return res;
     }
     
+    // O(2N)
     public int top() {
         int res = this.pop();
         q1.offer(res);
         return res;
     }
     
+    // O(1)
     public boolean empty() {
         return q1.isEmpty() && q2.isEmpty();
     }
