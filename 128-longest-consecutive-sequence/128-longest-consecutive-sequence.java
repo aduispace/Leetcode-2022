8e39404e52836cebd9fde7c464d4705b812f5f88
DLCoder <lindui1994@gmail.com>
2022-03-12 02:34:08 -0800

    Time: 34 ms (67.05%), Space: 61.2 MB (51.43%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -8,6 +8,7 @@ class Solution {
         
         for (int i : set) {
             int curRes = 0;
+            // 这里要加这个优化，不然会超时，本质是只从每个consecutive nums的start num开始找
             if (set.contains(i - 1)) {
                 continue;
             } else {
