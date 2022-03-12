d8fbe09e106b448f8b6a8e00a6506ff974674486
DLCoder <lindui1994@gmail.com>
2022-03-11 21:14:59 -0800

    Time: 0 ms (100.00%), Space: 41 MB (61.07%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,5 +1,7 @@
 class Solution {
     public void rotate(int[][] matrix) {
+        // just use reference, no need to return anything
+        // 先镜像，再每行反转
         transpose(matrix);
         reverse(matrix);
     }
@@ -19,6 +21,7 @@ class Solution {
     private void reverse(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i < n; i++) {
+            // 注意两个变量for loop写法
             for (int j = 0, k = n - 1; j <= k; j++, k--) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[i][k];
