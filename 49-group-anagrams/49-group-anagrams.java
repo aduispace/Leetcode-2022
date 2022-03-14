1a6ea0c108c4c63ed70719e7e2b4b9bc20349b74
DLCoder <lindui1994@gmail.com>
2022-03-14 00:38:05 -0700

    Time: 10 ms (82.25%), Space: 55.7 MB (49.43%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,5 +1,8 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+//         Time Complexity: O(NK \log K)O(NKlogK), where NN is the length of strs, and KK is the maximum length of a string in strs. The outer loop has complexity O(N)O(N) as we iterate through each string. Then, we sort each string in O(K \log K)O(KlogK) time.
+
+// Space Complexity: O(NK)O(NK), the total information content stored in ans.
         List<List<String>> res = new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
@@ -18,6 +21,7 @@ class Solution {
         // O(nlogN)
         char[] arr = s.toCharArray();
         Arrays.sort(arr);
+        // careful how convert array to string
         return String.valueOf(arr);
     }
 }
