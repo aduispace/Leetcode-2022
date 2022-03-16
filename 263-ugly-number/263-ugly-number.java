1e38c39560e02cf3042a5b2d0e4281c6c14acdf0
DLCoder <lindui1994@gmail.com>
2022-03-15 17:44:54 -0700

    Time: 1 ms (95.82%), Space: 42 MB (5.60%) - LeetHub

--- 263-ugly-number/263-ugly-number.java
+++ 263-ugly-number/263-ugly-number.java
@@ -1,13 +1,18 @@
 class Solution {
     public boolean isUgly(int n) {
-        int[] primes = new int[]{2, 3, 5};
-        for (int i = 0; n > 1 && i < 3; i++) {
-            if (n % primes[i] == 0) {
-                n /= primes[i];
-                i--;
-            }
-        }
+//         int[] primes = new int[]{2, 3, 5};
+//         for (int i = 0; n > 1 && i < 3; i++) {
+//             if (n % primes[i] == 0) {
+//                 n /= primes[i];
+//                 i--;
+//             }
+//         }
         
+//         return n == 1;
+        if (n == 0) return false;
+	    while(n % 2 == 0) n /= 2;
+	    while(n % 3 == 0) n /= 3;
+	    while(n % 5 == 0) n /= 5;
         return n == 1;
     }
 }
