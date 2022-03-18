3beef9f282228faaa1b11e3de767f41d62f6e702
DLCoder <lindui1994@gmail.com>
2022-03-18 00:27:13 -0700

    Time: 117 ms (21.61%), Space: 135.6 MB (28.08%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.java
@@ -0,0 +1,41 @@
+class LRUCache {
+    int capacity = 0;
+    Map<Integer, Integer> cache = new LinkedHashMap<>();
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        if (!cache.containsKey(key)) {
+            return -1;
+        }
+        int val = cache.get(key);
+        this.makeKeyMostRecently(key, val);
+        return val;
+    }
+    
+    public void put(int key, int value) {
+        this.makeKeyMostRecently(key, value);
+        if (cache.size() > capacity) {
+            Integer deleteKey = 0;
+            int i = cache.keySet().iterator().next();
+            cache.remove(i);
+        }
+    }
+    
+    private void makeKeyMostRecently(int key, int val) {
+        if (cache.containsKey(key)) {
+            // if key exists, it would be just an update
+            cache.remove(key);
+        }
+        cache.put(key, val);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
