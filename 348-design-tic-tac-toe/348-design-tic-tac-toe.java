5a49489be934b43062c762546c89870d69f406ee
DLCoder <lindui1994@gmail.com>
2022-03-23 01:35:33 -0700

    Time: 7 ms (34.77%), Space: 47.7 MB (52.88%) - LeetHub

--- 348-design-tic-tac-toe/348-design-tic-tac-toe.java
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.java
@@ -5,36 +5,27 @@ class TicTacToe {
     int anti = 0;
     
     // win needs every single spot in that col/row/diagonal/antiDigonal to be taken
-    int[] rowsTaken;
-    int[] colsTaken;
-    int diagonalTaken = 0;
-    int antiTaken = 0;
-    
+
     int n = 0;
     public TicTacToe(int n) {
         this.rows = new int[n];
         this.cols = new int[n];
-        this.rowsTaken = new int[n];
-        this.colsTaken = new int[n];
         this.n = n;
     }
     
     public int move(int row, int col, int player) {
-        int winCondition = player * n;
+        int winCondition = player == 1 ? -player * n : player * n;
+        int addOnNum = player == 1? -player : player;
         if (row == col) {
-            diagonal += player;
-            diagonalTaken++;
+            diagonal += addOnNum;
         } 
         if (row + col == n - 1) {
-            anti += player;
-            antiTaken++;
+            anti += addOnNum;
         }
-        rows[row] += player;
-        rowsTaken[row]++;
-        cols[col] += player;
-        colsTaken[col]++;
+        rows[row] += addOnNum;
+        cols[col] += addOnNum;
         
-        if ((diagonal == winCondition && diagonalTaken == n) || (anti == winCondition && antiTaken == n) || (rows[row] == winCondition && rowsTaken[row] == n) || (cols[col] == winCondition && colsTaken[col] == n)) {
+        if (diagonal == winCondition || anti == winCondition || rows[row] == winCondition || cols[col] == winCondition) {
             return player;
         }
         
