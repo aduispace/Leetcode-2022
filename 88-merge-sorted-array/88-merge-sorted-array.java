ea6fb46b6602a46abd6ae763f8f625783df2dba9
DLCoder <lindui1994@gmail.com>
2022-03-24 21:54:43 -0700

    Time: 0 ms (100.00%), Space: 42 MB (85.83%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // [7, 9, 10, 0, 0, 0] [3, 5, 6]
+        int i = m - 1, j = n - 1;
+        for (int k = m + n - 1; k >= 0; k--) {
+            if (j < 0) {
+                break;
+            }
+            if (i < 0) {
+                nums1[k] = nums2[j];
+                j--;
+                continue;
+            }
+            if (nums2[j] >= nums1[i]) {
+                nums1[k] = nums2[j];
+                j--;
+            } else {
+                nums1[k] = nums1[i];
+                i--;
+            }
+        }
+    }
+}
