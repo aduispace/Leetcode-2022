28a719620d978928a6a246c6276e4d58a0834793
DLCoder <lindui1994@gmail.com>
2022-03-27 16:58:15 -0700

    Time: 8 ms (30.82%), Space: 42.2 MB (50.78%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String reorganizeString(String s) {
+        // [aaaaaac]
+        char[] res = new char[s.length()];
+        Map<Character, Integer> map = new HashMap<>();
+        Queue<Pair<Character, Integer>> maxPQ = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        
+        for (char i : s.toCharArray()) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        
+        for (char key : map.keySet()) {
+            maxPQ.offer(new Pair<Character, Integer>(key, map.get(key)));
+        }
+        
+        int i = 0;
+        while (!maxPQ.isEmpty()) {
+            Pair<Character, Integer> first = maxPQ.poll();
+            if (i == 0 || res[i - 1] != first.getKey()) {
+                res[i] = first.getKey();
+                int count = first.getValue() - 1;
+                if (count > 0) maxPQ.offer(new Pair<Character, Integer>(res[i], count));
+            } else {
+                if (maxPQ.isEmpty()) return "";
+                Pair<Character, Integer> second = maxPQ.poll();
+                res[i] = second.getKey();
+                int count = second.getValue() - 1;
+                if (count > 0) maxPQ.offer(new Pair<Character, Integer>(res[i], count));
+                maxPQ.offer(first);
+            }
+            i++;
+        }
+        
+        return new String(res);
+    }
+}
