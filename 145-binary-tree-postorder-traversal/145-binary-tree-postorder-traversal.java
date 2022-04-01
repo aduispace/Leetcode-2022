598f72dd627a6f9df9c1f64321bcae806f3af067
DLCoder <lindui1994@gmail.com>
2022-04-01 01:59:07 -0700

    Time: 1 ms (44.76%), Space: 41.9 MB (70.21%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -13,17 +13,49 @@
  *     }
  * }
  */
+
+/*
+1. Iteration: 
+**/
+
 class Solution {
+        // postOrder: left -> right -> root
+        // preorder: root -> left -> right, so we could do root -> right -> left and revert the result to be: left -> right -> root, which is postorder
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        postOrder(root, res);
+        if (root == null) return res;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            res.add(cur.val);
+            // left first in, last out
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            // right last in, first out
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+        }
+        Collections.reverse(res);
+        
         return res;
     }
-    
-    private void postOrder(TreeNode root, List<Integer> res) {
-        if (root == null) return;
-        postOrder(root.left, res);
-        postOrder(root.right, res);
-        res.add(root.val);
-    }
 }
+
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         postOrder(root, res);
+//         return res;
+//     }
+    
+//     private void postOrder(TreeNode root, List<Integer> res) {
+//         if (root == null) return;
+//         postOrder(root.left, res);
+//         postOrder(root.right, res);
+//         res.add(root.val);
+//     }
+// }
