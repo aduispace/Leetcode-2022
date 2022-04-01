a871842b9931424519d2342dd9c32e3ee0344b9a
DLCoder <lindui1994@gmail.com>
2022-04-01 01:15:49 -0700

    Time: 1 ms (43.05%), Space: 42.8 MB (10.76%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -13,17 +13,48 @@
  *     }
  * }
  */
+
+/*
+1. Iteration: 
+**/
+
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+        // inorder: left -> root -> right
+        // 中序遍历顺序: 左-中-右 入栈顺序： 左-右
+        // inorder needs an addition pointer to handle adding result
         List<Integer> res = new ArrayList<>();
-        inOrder(root, res);
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode node = stack.pop();
+                res.add(node.val);
+                cur = node.right;
+            }
+        }
+        
         return res;
     }
-    
-    private void inOrder(TreeNode root, List<Integer> res) {
-        if (root == null) return;
-        inOrder(root.left, res);
-        res.add(root.val);
-        inOrder(root.right, res);
-    }
 }
+
+
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         inOrder(root, res);
+//         return res;
+//     }
+    
+//     private void inOrder(TreeNode root, List<Integer> res) {
+//         if (root == null) return;
+//         inOrder(root.left, res);
+//         res.add(root.val);
+//         inOrder(root.right, res);
+//     }
+// }
