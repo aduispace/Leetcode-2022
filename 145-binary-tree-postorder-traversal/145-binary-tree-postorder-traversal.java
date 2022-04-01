c34ed3167719bcdf3fc974474f83757d715ddc4a
DLCoder <lindui1994@gmail.com>
2022-04-01 02:00:50 -0700

    Time: 0 ms (100.00%), Space: 40.3 MB (93.84%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -21,6 +21,7 @@
 class Solution {
         // postOrder: left -> right -> root
         // preorder: root -> left -> right, so we could do root -> right -> left and revert the result to be: left -> right -> root, which is postorder
+    // 后序遍历，先序遍历是中左右，后续遍历是左右中，那么我们只需要调整一下先序遍历的代码顺序，就变成中右左的遍历顺序，然后在反转result数组，输出的结果顺序就是左右中了
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) return res;
@@ -39,6 +40,8 @@ class Solution {
                 stack.push(cur.right);
             }
         }
+        
+        // reverse order at last
         Collections.reverse(res);
         
         return res;
