e252f202cb45a684197280ceee23046786355f37
DLCoder <lindui1994@gmail.com>
2022-04-01 01:03:09 -0700

    Time: 1 ms (44.77%), Space: 42.7 MB (16.70%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -15,15 +15,39 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+        // preorder: root -> left -> right
+        // stack entrance sequence: root -> right -> left, first in last out!!!
+        Deque<TreeNode> stack = new ArrayDeque<>();
         List<Integer> res = new ArrayList<>();
-        preOrder(root, res);
+        if (root == null) return res; 
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            res.add(cur.val);
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+        
         return res;
     }
-    
-    private void preOrder(TreeNode root, List<Integer> res) {
-        if (root == null) return;
-        res.add(root.val);
-        preOrder(root.left, res);
-        preOrder(root.right, res);
-    }
 }
+
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         preOrder(root, res);
+//         return res;
+//     }
+    
+//     private void preOrder(TreeNode root, List<Integer> res) {
+//         if (root == null) return;
+//         res.add(root.val);
+//         preOrder(root.left, res);
+//         preOrder(root.right, res);
+//     }
+// }
