0b364b974c07c0ca85ff16c270b0a2b503c3f1c1
DLCoder <lindui1994@gmail.com>
2022-04-04 01:33:28 -0700

    Time: 1 ms (93.68%), Space: 42.4 MB (74.07%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -13,25 +13,46 @@
  *     }
  * }
  */
+// DFS: go right first then left,
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
         List<Integer> res = new ArrayList<>();
         if (root == null) return res;
-        queue.offer(root);
-        
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode cur = queue.poll();
-                if (i == size - 1) {
-                    res.add(cur.val);
-                }
-                if (cur.left != null) queue.offer(cur.left);
-                if (cur.right != null) queue.offer(cur.right);
-            }
-        }
+        dfs(root, 0, res);
         
         return res;
     }
+    
+    private void dfs(TreeNode root, int depth, List<Integer> res) {
+        if (depth == res.size()) {
+            res.add(root.val);
+        }
+        
+        if (root.right != null) dfs(root.right, depth + 1, res);
+        if (root.left != null) dfs(root.left, depth + 1, res);
+    }
 }
+
+// BFS: Level order traverse
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         List<Integer> res = new ArrayList<>();
+//         if (root == null) return res;
+//         queue.offer(root);
+        
+//         while (!queue.isEmpty()) {
+//             int size = queue.size();
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode cur = queue.poll();
+//                 if (i == size - 1) {
+//                     res.add(cur.val);
+//                 }
+//                 if (cur.left != null) queue.offer(cur.left);
+//                 if (cur.right != null) queue.offer(cur.right);
+//             }
+//         }
+        
+//         return res;
+//     }
+// }
