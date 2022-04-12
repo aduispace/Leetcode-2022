127adedb260c2575cfeb467bc3984406aac2a437
DLCoder <lindui1994@gmail.com>
2022-04-11 22:44:08 -0700

    Time: 3 ms (86.51%), Space: 43.1 MB (74.17%) - LeetHub

---
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        Queue<Node> queue = new LinkedList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        int leftMost = 0;
+        int rightMost = 0;
+        queue.offer(new Node(root, 0));
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node cur = queue.poll();
+                if (!map.containsKey(cur.level)) {
+                    map.put(cur.level, new ArrayList<Integer>());
+                } 
+                map.get(cur.level).add(cur.node.val);
+                if (cur.node.left != null) {
+                    queue.offer(new Node(cur.node.left, cur.level - 1));
+                    leftMost = Math.min(leftMost, cur.level - 1);
+                }
+                if (cur.node.right != null) {
+                    queue.offer(new Node(cur.node.right, cur.level + 1));
+                    rightMost = Math.max(rightMost, cur.level + 1);
+                }
+            }
+        }
+        
+        for (int i = leftMost; i <= rightMost; i++) {
+            res.add(map.get(i));
+        }
+        
+        return res;
+    }
+    
+    private class Node {
+        int level;
+        TreeNode node;
+        public Node(TreeNode node, int lvl) {
+            this.node = node;
+            this.level = lvl;
+        }
+    }
+}
