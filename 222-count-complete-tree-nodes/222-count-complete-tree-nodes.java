6cc4a3b1a144b73a7f07bfc969b5d12441bed959
DLCoder <lindui1994@gmail.com>
2022-04-15 17:52:20 -0700

    Time: 0 ms (100.00%), Space: 45.1 MB (84.69%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -14,11 +14,13 @@
  * }
  */
 class Solution {
+    // 
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
         
         TreeNode left = root, right = root;
         int leftHeight = 0, rightHeight = 0;
+        // time: O(logN), 树的高度 O(logN)
         while (left != null) {
             leftHeight++;
             left = left.left;
@@ -27,7 +29,7 @@ class Solution {
             rightHeight++;
             right = right.right;
         }
-        // check if this is a full perfect binary tree
+        // check if this is a full perfect binary tree, if yes get it in O(1), otherwise iterate over it would be O(logN) * O(logN), 由于完全二叉树的性质，其子树一定有一棵是满的，所以一定会触发 hl == hr，只消耗 O(logN) 的复杂度而不会继续递归.
         if (leftHeight == rightHeight) {
             return (int) Math.pow(2, leftHeight) - 1;
         } else {
