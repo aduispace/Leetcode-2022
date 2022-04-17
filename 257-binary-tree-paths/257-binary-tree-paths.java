c235afd678f9d658e01fc189b9dcb26f81c63e8b
DLCoder <lindui1994@gmail.com>
2022-04-16 21:55:50 -0700

    Time: 9 ms (53.47%), Space: 43 MB (70.25%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    // 回溯做法，本质是traverse
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> path = new ArrayList<>();
         List<String> res = new ArrayList<>();
@@ -39,8 +40,12 @@ class Solution {
             return;
         }
         if (root.left != null) {
+            // 前序位置
             path.add(root.val + "");
             traverse(root.left, path, res);
+            // 后序位置
+            // 这个解法应该很好理解，但为什么需要在前序位置增加 depth，在后序位置减小 depth？
+// 因为前面说了，前序位置是进入一个节点的时候，后序位置是离开一个节点的时候，depth 记录当前递归到的节点深度，你把 traverse 理解成在二叉树上游走的一个指针，所以当然要这样维护。
             path.remove(path.size() - 1);
         }
         if (root.right != null) {
