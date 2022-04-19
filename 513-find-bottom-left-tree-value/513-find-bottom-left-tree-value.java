413c7116d9f4490f6c05a38e606365dcd7a14844
DLCoder <lindui1994@gmail.com>
2022-04-18 18:54:04 -0700

    Time: 0 ms (100.00%), Space: 41.8 MB (90.45%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.java
@@ -14,26 +14,50 @@
  * }
  */
 class Solution {
+    private int maxDepth = 0;
+    private TreeNode res = null;
+    
     public int findBottomLeftValue(TreeNode root) {
-        // the leftmost value in the last row of the tree, the last row告诉我们可以bfs
-        Queue<TreeNode> queue = new LinkedList<>();
         if (root == null) return 0;
-        TreeNode leftMost = root;
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode cur = queue.poll();
-                if (i == 0) leftMost = cur;
-                if (cur.left != null) {
-                    queue.offer(cur.left);
-                }
-                if (cur.right != null) {
-                    queue.offer(cur.right);
-                }
-            }
+        traverse(root, 0);
+        return res.val;
+    }
+    
+    private void traverse(TreeNode root, int depth) {
+        if (root == null) return;
+        depth++;
+        if (depth > maxDepth) {
+            res = root;
+            maxDepth = depth;
         }
-        
-        return leftMost.val;
+        traverse(root.left, depth);
+        traverse(root.right, depth);
+        depth--;
     }
 }
+// BFS solution: 
+
+// class Solution {
+//     public int findBottomLeftValue(TreeNode root) {
+//         // the leftmost value in the last row of the tree, the last row告诉我们可以bfs
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         if (root == null) return 0;
+//         TreeNode leftMost = root;
+//         queue.offer(root);
+//         while (!queue.isEmpty()) {
+//             int size = queue.size();
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode cur = queue.poll();
+//                 if (i == 0) leftMost = cur;
+//                 if (cur.left != null) {
+//                     queue.offer(cur.left);
+//                 }
+//                 if (cur.right != null) {
+//                     queue.offer(cur.right);
+//                 }
+//             }
+//         }
+        
+//         return leftMost.val;
+//     }
+// }
