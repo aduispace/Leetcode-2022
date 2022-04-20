e0f1248c2d2991874992f120bd8810cd8d997df1
DLCoder <lindui1994@gmail.com>
2022-04-19 22:47:17 -0700

    Time: 4 ms (52.97%), Space: 44.4 MB (45.09%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -17,8 +17,9 @@ class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         return buildSubTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
-    // preorder [root, leftTree, rightTree]
-    // inorder [leftTree, root, rightTree]
+    // preorder partition [root, leftTree, rightTree]
+    // inorder partition [leftTree, root, rightTree]
+    // divide conquer 一般有返回值
     private TreeNode buildSubTree(int[] preorder, int preOrderLeft, int preOrderRight, int[] inorder, int inOrderLeft, int inOrderRight) {
         if (preOrderLeft > preOrderRight || preOrderLeft > preorder.length - 1) return null; 
         int rootVal = preorder[preOrderLeft];
@@ -38,7 +39,8 @@ class Solution {
         int newRightInOrderLeft = pos + 1;
         int newRightInOrderRight = inOrderRight;
         
-        
+        // divide and conquer: 二叉树的构造问题一般都是使用「分解问题」的思路：构造整棵树 = 根节点 + 构造左子树 + 构造右子树。
+
         root.left = buildSubTree(preorder, newLeftPreOrderLeft, newLeftPreOrderRight, inorder, newLeftInOrderLeft, newLeftInOrderRight);
         root.right = buildSubTree(preorder, newRightPreOrderLeft, newRightPreOrderRight, inorder, newRightInOrderLeft, newRightInOrderRight);
         
