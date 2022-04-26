70108211ab7144f3298d62d56528a4bfdc025134
DLCoder <lindui1994@gmail.com>
2022-04-25 22:25:42 -0700

    Time: 0 ms (100.00%), Space: 43.5 MB (57.69%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
@@ -22,6 +22,10 @@ class Solution {
         if (nums == null || nums.length == 0) return null;
         if (left > right) return null;
         
+//         首先取数组中间元素的位置，不难写出int mid = (left + right) / 2;，这么写其实有一个问题，就是数值越界，例如left和right都是最大int，这么操作就越界了，在二分法 (opens new window)中尤其需要注意！
+// 所以可以这么写：int mid = left + ((right - left) / 2);
+        
+        
         int mid = left + (right - left) / 2;
         TreeNode root = new TreeNode(nums[mid]);
         root.left = buildBST(nums, left, mid - 1);
