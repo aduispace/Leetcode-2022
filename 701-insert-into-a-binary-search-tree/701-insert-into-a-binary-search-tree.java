406dff901ae9a2ce31afacf5e2475116e44d3f23
DLCoder <lindui1994@gmail.com>
2022-04-26 00:46:05 -0700

    Time: 0 ms (100.00%), Space: 43.1 MB (78.83%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
@@ -16,8 +16,11 @@
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
         if (root == null) {
-            root = new TreeNode(val);
-            return root;
+//             1、寻找正确的插入位置，类似 700. 二叉搜索树中的搜索。
+
+// 2、把元素插进去，这就要把新节点以返回值的方式接到父节点上。
+            
+            return new TreeNode(val);
         }
         if (val < root.val) {
             root.left = insertIntoBST(root.left, val);
