25cbd835e8b7ddf3d665e2042ec8662b6a8ee086
DLCoder <lindui1994@gmail.com>
2022-04-25 22:08:30 -0700

    Time: 0 ms (100.00%), Space: 45.6 MB (6.31%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
@@ -17,6 +17,7 @@ class Solution {
     // divide and conquer
     public TreeNode trimBST(TreeNode root, int low, int high) {
         if (root == null) return root;
+        // root为本层处理的结点
         if (root.val < low) {
             // 直接返回 root.right
             // 等于删除 root 以及 root 的左子树
@@ -26,6 +27,7 @@ class Solution {
             // 等于删除 root 以及 root 的右子树
             root = trimBST(root.left, low, high);
         } else {
+            // 闭区间 [lo, hi] 内的节点什么都不做
             root.left = trimBST(root.left, low, high);
             root.right = trimBST(root.right, low, high);
         }
