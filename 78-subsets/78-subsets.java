0f661b2cb80039b9d43d7d6008992ab64e43e74f
DLCoder <lindui1994@gmail.com>
2022-04-27 21:14:11 -0700

    Time: 1 ms (78.72%), Space: 43.4 MB (41.62%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> path = new ArrayList<>();
+        backtrack(nums, path, 0);
+        
+        return res;
+    }
+    
+    // subset因为元素不能重复使用，backtrack tree结构不一样
+    
+    private void backtrack(int[] nums, List<Integer> path, int start) {
+        res.add(new ArrayList<Integer>(path));
+        
+        for (int i = start; i < nums.length; i++) {
+            path.add(nums[i]);
+            backtrack(nums, path, i + 1);
+            path.remove(path.size() - 1);
+        }
+        
+        return;
+    }
+}
