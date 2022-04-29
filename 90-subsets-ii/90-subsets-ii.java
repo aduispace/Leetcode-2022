f6cbbb6790c62e84807d10cd1c771d3d9e3f8dc3
DLCoder <lindui1994@gmail.com>
2022-04-29 01:35:08 -0700

    Time: 2 ms (76.23%), Space: 44.3 MB (39.37%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -12,7 +12,8 @@ class Solution {
         res.add(new ArrayList<Integer>(path));
         
         for (int i = start; i < nums.length; i++) {
-            if (i != start && nums[i - 1] == nums[i]) {
+            // 当前层的选择，第一个可以进去，第二个开始判断如果一样就不进去
+            if (i > start && nums[i - 1] == nums[i]) {
                 continue;
             }
             path.add(nums[i]);
