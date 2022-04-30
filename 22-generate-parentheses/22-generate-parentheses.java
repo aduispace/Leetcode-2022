eaba4fee1b5cecdbc4c59e71bceafda12997138e
DLCoder <lindui1994@gmail.com>
2022-04-30 10:18:29 -0700

    Time: 1 ms (93.26%), Space: 42.3 MB (84.08%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+    
+    public List<String> generateParenthesis(int n) {
+        backtrack(new StringBuilder(), n, n);
+        
+        return res;
+    }
+    
+    private void backtrack(StringBuilder path, int left, int right) {
+        if (left == 0 && right == 0) {
+            res.add(path.toString());
+            return;
+        } else if (left > right) {
+            return;
+        } else if (left < 0 || right < 0) {
+            return;
+        }
+        
+        path.append("(");
+        backtrack(path, left - 1, right);
+        path.setLength(path.length() - 1);
+        
+        path.append(")");
+        backtrack(path, left, right - 1);
+        path.setLength(path.length() - 1);
+        
+        return;
+    }
+}
