9491d6d9f24f8c6be1476577ed6ace43f0b1172e
DLCoder <lindui1994@gmail.com>
2022-05-01 19:04:31 -0700

    Time: 3 ms (79.28%), Space: 55.1 MB (45.11%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -0,0 +1,29 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {       
+        traverse(graph, new ArrayList<Integer>(), 0);
+        return res;
+    }
+    
+    // acyclic, so no need to add visited metrics to track
+    private void traverse(int[][] graph, List<Integer> path, int start) {
+        // end condition, and edge cases
+        if (start == graph.length - 1) {
+            path.add(start);
+            res.add(new ArrayList<>(path));
+            path.remove(path.size() - 1);
+            return;
+        } else if (graph.length == 0) {
+            return;
+        }
+        
+        path.add(start);
+        for (int i : graph[start]) {
+            traverse(graph, path, i);
+        }
+        path.remove(path.size() - 1);
+        
+        return;
+    }
+}
