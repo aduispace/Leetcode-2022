042b771756a38b7cabd202bfded07973f8181830
DLCoder <lindui1994@gmail.com>
2022-05-06 00:39:36 -0700

    Time: 0 ms (100.00%), Space: 45.5 MB (24.22%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -13,6 +13,10 @@
  *     }
  * }
  */
+
+// Click here to learn how to insert a node into a BST and delete a node from a BST, the time complexity of these operations is O(H)O(H), where HH is a height of the binary tree. H = \log NH=logN for the balanced tree and H = NH=N for a skewed tree.
+
+
 class Solution {
     int res;
     int cur = 0; 
@@ -23,12 +27,14 @@ class Solution {
     }
     
     // 这里不能带着cur int进traverse，因为int不是reference type而是primitive type
+    // Time: O(N) when 
     private void traverse(TreeNode root, int k) {
         if (root == null) return;
         traverse(root.left, k);
         cur++;
         if (cur == k) {
             res = root.val;
+            return;
         }
         traverse(root.right, k);
         
