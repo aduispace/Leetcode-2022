0dc2b17ca32faa0815e33ac4f38587c937af26e3
DLCoder <lindui1994@gmail.com>
2022-05-07 02:03:31 -0700

    Time: 107 ms (64.80%), Space: 80.3 MB (12.15%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
@@ -4,23 +4,23 @@ class Solution {
             return false;
         }
         
-        Map<Integer, Integer> treeMap = new TreeMap<>((a, b) -> a - b);
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i : nums) {
-            treeMap.put(i, treeMap.getOrDefault(i, 0) + 1);
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
+    
+        // hashMap + sorted array    
+        Arrays.sort(nums); // go through every number of nums, not the keySet()
         
-        // [1: 0, 2: 0, 3: 1, 4: 1, 5: 1, 6: 1] --- 
-        
-        // [1, 2, 3, 4]
-        
+        // approach 2: use a treeMap
         // j decrement from k - 1 is because the number from back should be more than number from front
-        for (int i : treeMap.keySet()) {
-            if (treeMap.get(i) > 0) {
-                for (int j = k - 1; j >= 0; j--) {
-                    if (!treeMap.containsKey(i + j) || treeMap.get(i + j) < treeMap.get(i)) {
+        for (int i : nums) {
+            if (map.get(i) > 0) {
+                for (int j = 0; j < k; j++) {
+                    if (!map.containsKey(i + j) || map.get(i + j) == 0) {
                         return false;
                     } else {
-                        treeMap.put(i + j, treeMap.get(i + j) - treeMap.get(i));
+                        map.put(i + j, map.get(i + j) - 1);
                     }
                 }
             }
