489b47df87820962007e536fe895c7ddf08541bd
DLCoder <lindui1994@gmail.com>
2022-05-09 22:35:52 -0700

    Time: 0 ms (100.00%), Space: 42.3 MB (32.31%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,21 +1,20 @@
 class Solution {
+    int i = 0;
+    
     public String decodeString(String s) {
-        Queue<Character> queue = new LinkedList<>();
-        for (char i : s.toCharArray()) {
-            queue.offer(i);
-        }
-        return build(queue);
+        return build(s);
     }
     
-    private String build(Queue<Character> queue) {
+    private String build(String s) {
         StringBuilder sb = new StringBuilder();
         int num = 0;
-        while (!queue.isEmpty()) {
-            char ch = queue.poll();
+        while (i < s.length()) {
+            char ch = s.charAt(i);
+            i++;
             if (Character.isDigit(ch)) {
                 num = num * 10 + (ch - '0');
             } else if (ch == '[') {
-                String cur = build(queue);
+                String cur = build(s);
                 for (int j = 0; j < num; j++) {
                     sb.append(cur);
                 }
@@ -30,3 +29,35 @@ class Solution {
         return sb.toString();
     }
 }
+
+
+//     public String decodeString(String s) {
+//         Queue<Character> queue = new LinkedList<>();
+//         for (char i : s.toCharArray()) {
+//             queue.offer(i);
+//         }
+//         return build(queue);
+//     }
+    
+//     private String build(Queue<Character> queue) {
+//         StringBuilder sb = new StringBuilder();
+//         int num = 0;
+//         while (!queue.isEmpty()) {
+//             char ch = queue.poll();
+//             if (Character.isDigit(ch)) {
+//                 num = num * 10 + (ch - '0');
+//             } else if (ch == '[') {
+//                 String cur = build(queue);
+//                 for (int j = 0; j < num; j++) {
+//                     sb.append(cur);
+//                 }
+//                 num = 0;
+//             } else if (Character.isAlphabetic(ch)) {
+//                 sb.append(ch);
+//             } else if (ch == ']') {
+//                 break;
+//             }
+//         }
+        
+//         return sb.toString();
+//     }
