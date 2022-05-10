3de4a9b9ffd96549c73f6698d550efcd66c1b001
DLCoder <lindui1994@gmail.com>
2022-05-09 23:34:07 -0700

    Time: 3 ms (41.22%), Space: 42 MB (58.26%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,42 +1,92 @@
 // Approach 1: Divide and Conquer, DFS
 
+// class Solution {
+//     // Time: O(K*N)
+//     // use a pointer from beginning
+//     int i = 0;
+    
+//     public String decodeString(String s) {
+//         return build(s);
+//     }
+    
+//     private String build(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         int num = 0;
+//         while (i < s.length()) {
+//             char ch = s.charAt(i);
+//             i++;
+//             if (Character.isDigit(ch)) {
+//                 num = num * 10 + (ch - '0');
+//             } else if (ch == '[') {
+//                 // use recursion build(s), append that k times
+//                 String cur = build(s);
+                
+//                 // O(K), append k times 
+//                 for (int j = 0; j < num; j++) {
+//                     sb.append(cur);
+//                 }
+//                 // reset num to 0
+//                 num = 0;
+//             } else if (Character.isAlphabetic(ch)) {
+//                 sb.append(ch);
+//             } else if (ch == ']') {
+//                 break;
+//             }
+//         }
+        
+//         return sb.toString();
+//     }
+// }
+
+// Approach 2: Using stack: 
+
 class Solution {
     // Time: O(K*N)
-    // use a pointer from beginning
-    int i = 0;
-    
+
     public String decodeString(String s) {
-        return build(s);
-    }
-    
-    private String build(String s) {
+        Deque<String> strs = new ArrayDeque<>();
+        Deque<Integer> ints = new ArrayDeque<>();
+        
+        char[] arr = s.toCharArray();
         StringBuilder sb = new StringBuilder();
         int num = 0;
+        int i = 0;
         while (i < s.length()) {
-            char ch = s.charAt(i);
-            i++;
-            if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
-            } else if (ch == '[') {
-                // use recursion build(s), append that k times
-                String cur = build(s);
-                
-                // O(K), append k times 
-                for (int j = 0; j < num; j++) {
-                    sb.append(cur);
+            if (Character.isDigit(arr[i])) {
+                while (Character.isDigit(arr[i])) {
+                    num = num * 10 + arr[i] - '0';
+                    i++;
                 }
-                // reset num to 0
+            } else if (arr[i] == '[') {
+                ints.push(num);
                 num = 0;
-            } else if (Character.isAlphabetic(ch)) {
-                sb.append(ch);
-            } else if (ch == ']') {
-                break;
+                
+                strs.push(sb.toString());
+                sb = new StringBuilder();
+                i++;
+            } else if (arr[i] == ']') {
+                int k = ints.pop();
+                String temp = "";
+                String cur = sb.toString();
+                String top = strs.pop();
+                for (int j = 0; j < k; j++) {
+                    temp = temp + cur;
+                }
+                temp = top + temp;
+                
+                sb = new StringBuilder(temp);
+                i++;
+            } else {
+                sb.append(arr[i]);
+                i++;
             }
         }
         
         return sb.toString();
-    }
+    } 
+    
 }
+
 
 
 //     public String decodeString(String s) {
