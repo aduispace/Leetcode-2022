8df0fe635aee3e9e93455939a6a4b0efab119ce2
DLCoder <lindui1994@gmail.com>
2022-05-11 01:28:30 -0700

    Time: 18 ms (79.86%), Space: 65.1 MB (69.52%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.java
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -15,14 +15,8 @@ class Solution {
             }
         }
         
+        // res是能装两个的船个数，剩下的(people.length - 2 * start)为装一个的船的个数
         return res + (people.length - 2 * start);
     }
-    
-    // [1, 5, 4, 2] 6
-    // [1, 2, 4, 5] 2
-    
-    
-    // [2, 3, 4, 5] 5
-    
-    // [3, 3, 4, 5] 5
+
 }
