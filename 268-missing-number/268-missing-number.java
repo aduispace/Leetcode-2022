621503279ff42341f896b2a7432d29405dcb9c8d
DLCoder <lindui1994@gmail.com>
2022-05-13 14:57:32 -0700

    Time: 0 ms (100.00%), Space: 43.1 MB (82.36%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // 0 + 1 + 2 + 3 = 6
+        // Time O(N) with Space O(1) 
         int suppose = 0;
         int actual = 0;
         for (int i = 0; i <= nums.length; i++) {
