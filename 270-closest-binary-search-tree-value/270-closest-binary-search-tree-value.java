2b81cdc9faa490767451c4aadca95cbd61051af6
DLCoder <lindui1994@gmail.com>
2022-05-12 22:21:43 -0700

    Time: 0 ms (100.00%), Space: 43.6 MB (49.03%) - LeetHub

--- 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.java
+++ 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.java
@@ -14,8 +14,7 @@
  * }
  */
 class Solution {
-    double min = Double.MAX_VALUE;
-    int res;
+    int res = Integer.MAX_VALUE;
     
     public int closestValue(TreeNode root, double target) {
         traverse(root, target);
@@ -24,13 +23,35 @@ class Solution {
     
     private void traverse(TreeNode root, double target) {
         if (root == null) return;
-        traverse(root.left, target);
-        if (Math.abs(target - (double) root.val) < min) {
-            min = Math.abs(target - (double) root.val);
-            res = root.val;
+        res = Math.abs(root.val - target) < Math.abs(res - target) ? root.val : res;
+        if (root.val > target) {
+            traverse(root.left, target);
+        } else {
+            traverse(root.right, target);
         }
-        traverse(root.right, target);
-        
-        return;
     }
 }
+
+
+
+// class Solution {
+//     double min = Double.MAX_VALUE;
+//     int res;
+    
+//     public int closestValue(TreeNode root, double target) {
+//         traverse(root, target);
+//         return res;
+//     }
+    
+//     private void traverse(TreeNode root, double target) {
+//         if (root == null) return;
+//         traverse(root.left, target);
+//         if (Math.abs(target - (double) root.val) < min) {
+//             min = Math.abs(target - (double) root.val);
+//             res = root.val;
+//         }
+//         traverse(root.right, target);
+        
+//         return;
+//     }
+// }
