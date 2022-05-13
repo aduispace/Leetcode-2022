4fd08755ff62732b3decff7b574949da75010d8c
DLCoder <lindui1994@gmail.com>
2022-05-13 01:35:39 -0700

    Time: 11 ms (30.80%), Space: 44.7 MB (10.33%) - LeetHub

---
+++ 93-restore-ip-addresses/93-restore-ip-addresses.java
@@ -0,0 +1,39 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+    
+    public List<String> restoreIpAddresses(String s) {
+        generateIpAddress(new ArrayList<String>(), 0, s);
+        return res;
+    }
+    
+    private void generateIpAddress(List<String> path, int start, String s) {
+        if (start == s.length() && path.size() == 4) {
+            res.add(String.join(".", path)); 
+            return;
+        } 
+        for (int i = start; i <= s.length(); i++) {
+            String part = s.substring(start, i); // [start, i)
+            if (part.length() > 0 && isValid(part)) {
+                path.add(part);
+                generateIpAddress(path, i, s);
+                path.remove(path.size() - 1);
+            }
+        }
+        
+        return;
+    }
+    
+    private boolean isValid(String str) {
+        if (str.length() > 1 && str.charAt(0) == '0') {
+            return false;
+        }
+        if (str.length() > 3) {
+            return false;
+        }
+        if (Long.valueOf(str) > (long)255) {
+            return false;
+        }
+        
+        return true;
+    }
+}
