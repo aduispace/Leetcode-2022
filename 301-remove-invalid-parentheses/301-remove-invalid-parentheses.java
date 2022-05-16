dd0206f484f7741a7b15b245967ea2062a12cc21
DLCoder <lindui1994@gmail.com>
2022-05-15 18:32:10 -0700

    Time: 8 ms (78.53%), Space: 45.3 MB (38.39%) - LeetHub

---
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.java
@@ -0,0 +1,55 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+
+    public List<String> removeInvalidParentheses(String s) {
+        int left = 0, right = 0;
+        for (char c : s.toCharArray()) {
+            left += c == '(' ? 1 : 0;
+            if (left == 0) {
+                right += c == ')' ? 1 : 0;
+            } else {
+                left -= c == ')' ? 1 : 0;
+            }
+        }
+        getValidString(left, right, 0, s);
+        return res;
+    }
+    
+    private void getValidString(int left, int right, int start, String s) {
+        if (left == 0 && right == 0) {
+            if (isValid(s)) {
+                res.add(s);
+            }
+            return;
+        }
+        
+        for (int i = start; i < s.length(); i++) {
+            if (i > start && s.charAt(i) == s.charAt(i - 1)) {
+                continue;
+            } 
+            
+            if (left > 0 && s.charAt(i) == '(') {
+                getValidString(left - 1, right, i, s.substring(0, i) + s.substring(i + 1));
+            }
+            
+            if (right > 0 && s.charAt(i) == ')') {
+                getValidString(left, right - 1, i, s.substring(0, i) + s.substring(i + 1));
+            }
+        }
+        
+        return;
+    }
+        
+    private boolean isValid(String s) {
+        int leftCount = 0, rightCount = 0;
+        for (char c : s.toCharArray()) {
+            if (rightCount > leftCount) {
+                return false;
+            }
+            if (c == '(') leftCount++;
+            if (c == ')') rightCount++;
+        }
+        
+        return leftCount == rightCount;
+    }     
+}
