7d071b8ad28906f46abdb3991e2a39758b0781c9
DLCoder <lindui1994@gmail.com>
2022-05-15 22:23:32 -0700

    Time: 18 ms (45.03%), Space: 42.2 MB (26.93%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -0,0 +1,56 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        backtrack(board, 0, 0);
+        return;
+    }
+    
+    // board.length == 9
+    private boolean backtrack(char[][] board, int row, int col) {
+        if (row >= 9) {
+            return true;
+        }
+        
+        if (col >= 9) {
+            return backtrack(board, row + 1, 0);
+        }
+        
+        
+        if (board[row][col] != '.') {
+            return backtrack(board, row, col + 1);
+        }
+        
+
+        for (char ch = '1'; ch <= '9'; ch++) {
+            
+            if (isValid(board, row, col, ch)) {
+                board[row][col] = ch;
+                if (backtrack(board, row, col + 1)) {
+                    return true;
+                }
+                board[row][col] = '.';
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean isValid(char[][] board, int i, int j, char c) {
+        // check row and column
+        for (int temp = 0; temp < 9; temp++) {
+            if (board[i][temp] == c) {
+                return false;
+            }
+            if (board[temp][j] == c) {
+                return false;
+            }
+            
+            // check 3x3 square
+            if (board[(i/3)*3 + temp / 3][(j/3)*3 + temp % 3] == c) {
+                return false;
+            }
+        }
+
+        
+        return true;
+    }
+}
