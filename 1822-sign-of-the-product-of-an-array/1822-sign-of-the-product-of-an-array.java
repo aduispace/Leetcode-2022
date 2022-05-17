c6ba6e421e413a46fa56dadc697d81ceae7011ba
DLCoder <lindui1994@gmail.com>
2022-05-17 02:09:22 -0700

    Time: 1 ms (57.23%), Space: 43.7 MB (64.78%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int product = 1;
+        for (int i : nums) {
+            if (i > 0) {
+                product *= 1;
+            } else if (i < 0) {
+                product *= -1;
+            } else {
+                product *= 0;
+            }
+        }
+        
+        return product == 0 ? 0 : (product > 0 ? 1 : -1);
+    }
+}
