238576593f2511c6f8cd40fadfe56a9600a87b26
DLCoder <lindui1994@gmail.com>
2022-05-18 01:38:20 -0700

    Time: 2 ms (96.26%), Space: 40.8 MB (96.78%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,22 +1,44 @@
 class Solution {
+//     public boolean wordBreak(String s, List<String> wordDict) {
+//         Set<String> dict = new HashSet<>(wordDict);
+//         Map<String, Boolean> memo = new HashMap<>();
+//         return checkWordBreak(s, memo, dict);
+//     }
+    
+//     private boolean checkWordBreak(String s, Map<String, Boolean> memo, Set<String> dict) {
+//         if (memo.containsKey(s)) {
+//             return memo.get(s);
+//         }
+//         if (dict.contains(s)) {
+//             memo.put(s, true);
+//             return true;
+//         }
+//         for (int i = 0; i <= s.length(); i++) {
+//             String left = s.substring(0, i);
+//             String right = s.substring(i);
+//             if (dict.contains(left) && checkWordBreak(right, memo, dict)) {
+//                 memo.put(s, true);
+//                 return true;
+//             }
+//         }
+//         memo.put(s, false);
+//         return false;
+//     }
+    
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> dict = new HashSet<>(wordDict);
         Map<String, Boolean> memo = new HashMap<>();
-        return checkWordBreak(s, memo, dict);
+        return checkWordBreak(s, memo, wordDict);
     }
     
-    private boolean checkWordBreak(String s, Map<String, Boolean> memo, Set<String> dict) {
+    private boolean checkWordBreak(String s, Map<String, Boolean> memo, List<String> wordDict) {
         if (memo.containsKey(s)) {
             return memo.get(s);
         }
-        if (dict.contains(s)) {
-            memo.put(s, true);
+        if (s.length() == 0) {
             return true;
         }
-        for (int i = 0; i <= s.length(); i++) {
-            String left = s.substring(0, i);
-            String right = s.substring(i);
-            if (dict.contains(left) && checkWordBreak(right, memo, dict)) {
+        for (String word : wordDict) {
+            if (s.startsWith(word) && checkWordBreak(s.substring(word.length()), memo, wordDict)) {
                 memo.put(s, true);
                 return true;
             }
