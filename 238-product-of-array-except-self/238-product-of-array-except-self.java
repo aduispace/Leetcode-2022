9fe1059c4929a3a47306c50a827577ccb7bc0ed9
DLCoder <lindui1994@gmail.com>
2022-05-20 00:20:18 -0700

    Time: 4 ms (16.13%), Space: 57.3 MB (67.86%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,11 +1,14 @@
 class Solution {
+    // 
     public int[] productExceptSelf(int[] nums) {
         if (nums.length < 2) {
             return nums;
         }
         
+        // 从左到右的前缀积，prefix[i] 是 nums[0..i] 的元素积
         int[] prefix = new int[nums.length];
         prefix[0] = nums[0];
+        // 从右到左的前缀积，suffix[i] 是 nums[i..n-1] 的元素积
         int[] suffix = new int[nums.length];
         suffix[nums.length - 1] = nums[nums.length - 1];
         
@@ -19,7 +22,7 @@ class Solution {
         
         int[] res = new int[nums.length];
         
-        // initialize first and last in res
+        // 第一个和最后一个元素单独fill，initialize first and last in res
         res[0] = suffix[1];
         res[nums.length - 1] = prefix[nums.length - 2];
         
@@ -30,3 +33,17 @@ class Solution {
         return res;
     }
 }
+
+// easier version: 左右跑两遍，第一遍存prefix product，第二遍存suffix product， O(1) extra space
+// public int[] productExceptSelf(int[] nums) {
+//     int[] result = new int[nums.length];
+//     for (int i = 0, tmp = 1; i < nums.length; i++) {
+//         result[i] = tmp;
+//         tmp *= nums[i];
+//     }
+//     for (int i = nums.length - 1, tmp = 1; i >= 0; i--) {
+//         result[i] *= tmp;
+//         tmp *= nums[i];
+//     }
+//     return result;
+// }
