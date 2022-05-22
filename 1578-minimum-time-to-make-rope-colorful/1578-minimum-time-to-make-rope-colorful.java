b9bf4ce397e2f8fef385d4eea67f42fe061af086
DLCoder <lindui1994@gmail.com>
2022-05-22 16:28:27 -0700

    Time: 11 ms (59.86%), Space: 97.4 MB (23.56%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,5 +1,28 @@
+// For each group of continuous same characters,
+// we need cost = sum_cost(group) - max_cost(group)
+
+class Solution {
+    // Time O(N)
+    public int minCost(String s, int[] cost) {
+        int res = 0, max_cost = 0, sum_cost = 0, n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (i > 0 && s.charAt(i) != s.charAt(i - 1)) {
+                res += sum_cost - max_cost;
+                sum_cost = max_cost = 0;
+            }
+            sum_cost += cost[i];
+            max_cost = Math.max(max_cost, cost[i]);
+        }
+        res += sum_cost - max_cost;
+        return res;
+    }
+
+}
+/**
 class Solution {
     public int minCost(String colors, int[] neededTime) {
+        // O(NlogN)
+        
         colors = colors + '0';
         char[] arr = colors.toCharArray();
         int res = 0;
@@ -21,3 +44,5 @@ class Solution {
         return res;
     }
 }
+
+*/
