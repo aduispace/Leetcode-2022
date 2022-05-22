617a65f4c04e2bd7f45c2b46c145e9f4d85c4863
DLCoder <lindui1994@gmail.com>
2022-05-21 22:47:35 -0700

    Time: 19 ms (14.81%), Space: 65.6 MB (18.09%) - LeetHub

---
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int countServers(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 0;
+        boolean[][] visited = new boolean[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    int cur = dfs(visited, grid, i, j);
+                    res += cur > 1 ? cur : 0;
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    private int dfs(boolean[][] visited, int[][] grid, int x, int y) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 1;
+        visited[x][y] = true;
+        
+        for (int i = 0; i < m; i++) {
+            if (grid[i][y] == 1 && !visited[i][y]) {
+                res += dfs(visited, grid, i, y);
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (grid[x][i] == 1 && !visited[x][i]) {
+                res += dfs(visited, grid, x, i);
+            }
+        }
+        
+        return res;
+    }
+}
