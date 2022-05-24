41db5fbfd61ea02054317341c487cbd1de1619f8
DLCoder <lindui1994@gmail.com>
2022-05-24 16:05:46 -0700

    Time: 0 ms (100.00%), Space: 42.8 MB (49.00%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid + 1 < nums.length && nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return left;
+    }
+}
