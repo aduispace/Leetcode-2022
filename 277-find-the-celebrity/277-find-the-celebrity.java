5fbb34c561b5a090ab0a9283a45a70574d2d0e86
DLCoder <lindui1994@gmail.com>
2022-05-23 22:51:10 -0700

    Time: 26 ms (33.05%), Space: 54.5 MB (22.69%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.java
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -8,6 +8,7 @@ public class Solution extends Relation {
         int cand = 0;
         
         for (int i = 1; i < n; i++) {
+            // cand cannot know i, i must know cand, if false, switch the candidate
             if (knows(cand, i) || !knows(i, cand)) {
                 cand = i;
             }
@@ -24,3 +25,31 @@ public class Solution extends Relation {
         return cand;
     }
 }
+/***
+int findCelebrity(int n) {
+    // 先假设 cand 是名人
+    int cand = 0;
+    for (int other = 1; other < n; other++) {
+        if (!knows(other, cand) || knows(cand, other)) {
+            // cand 不可能是名人，排除
+            // 假设 other 是名人
+            cand = other;
+        } else {
+            // other 不可能是名人，排除
+            // 什么都不用做，继续假设 cand 是名人
+        }
+    }
+
+    // 现在的 cand 是排除的最后结果，但不能保证一定是名人
+    for (int other = 0; other < n; other++) {
+        if (cand == other) continue;
+        // 需要保证其他人都认识 cand，且 cand 不认识任何其他人
+        if (!knows(other, cand) || knows(cand, other)) {
+            return -1;
+        }
+    }
+
+    return cand;
+}
+
+**/
