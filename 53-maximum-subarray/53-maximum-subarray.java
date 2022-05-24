3336949c57254732cef3486f791a2c79374c99dd
DLCoder <lindui1994@gmail.com>
2022-05-23 21:58:25 -0700

    Time: 5 ms (5.34%), Space: 74.5 MB (30.21%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,15 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         // [-2, -1, -4, 0, -1, 1, 2, -3, 1]
-        
+        // O(N) dynamic programming
         int[] dp = new int[nums.length];
+        int res = nums[0];
+
         dp[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
             dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-        }
-        int res = Integer.MIN_VALUE;
-        
-        for (int i = 0; i < dp.length; i++) {
             res = Math.max(dp[i], res);
         }
         
