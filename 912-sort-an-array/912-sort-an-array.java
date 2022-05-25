29cd990e0222a08192479ebf007ca5fc34cf1b06
DLCoder <lindui1994@gmail.com>
2022-05-24 23:18:48 -0700

    Time: 1003 ms (15.62%), Space: 70.1 MB (37.82%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        // quick sort
-        
+        // quick sort: O(nlogN)
         quickSort(nums, 0, nums.length - 1);
         return nums;
     }
     
     private void quickSort(int[] nums, int low, int high) {
+        // baseCase
         if (nums.length == 0) {
             return;
         }
@@ -23,13 +23,16 @@ class Solution {
             while (left < right && pivot <= nums[right]) {
                 right--;
             }
+            // swap
             nums[left] = nums[right];
             // 从左往右找到比pivot大的，扔到右边，相等扔到左边右边都可以
             while (left < right && pivot >= nums[left]) {
                 left++;
             }
+            // swap
             nums[right] = nums[left];
         }
+        // put pivot back to its correct position
         nums[left] = pivot;
         
         quickSort(nums, low, left - 1);
