3db788543e7fa69396ca0b2648827efa3a507817
DLCoder <lindui1994@gmail.com>
2022-05-24 19:14:20 -0700

    Time: 17 ms (29.48%), Space: 45.3 MB (39.77%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,24 +1,49 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // sliding window:
-        Map<Character, Integer> rec = new HashMap<>();
-        int i = 0, j = 0;
-        int longest = 0;
-
+        Set<Character> set = new HashSet<>();
+        char[] arr = s.toCharArray();
+        int j = 0, i = 0;
+        int res = 0;
+        
         while (j < s.length()) {
-            char right = s.charAt(j);
-            rec.put(right, rec.getOrDefault(right, 0) + 1);
-            while (rec.getOrDefault(right, 0) > 1) {
-                char left = s.charAt(i);
-                rec.put(left, rec.getOrDefault(left, 0) - 1);
-                i++;
+            if (!set.contains(arr[j])) {
+                set.add(arr[j]);
+                j++;
+            } else {
+                while (set.contains(arr[j])) {
+                    set.remove(arr[i]);
+                    i++;
+                }
             }
-            j++;
-
-            
-            longest = Math.max(j - i, longest);
+            res = Math.max(res, j - i);
         }
         
-        return longest;
+        return res;
     }
 }
+
+
+
+
+//         // sliding window:
+//         Map<Character, Integer> rec = new HashMap<>();
+//         int i = 0, j = 0;
+//         int longest = 0;
+
+//         while (j < s.length()) {
+//             char right = s.charAt(j);
+//             rec.put(right, rec.getOrDefault(right, 0) + 1);
+//             // j多跑了一步，j在当前right后面一位
+//             j++;
+//             while (rec.getOrDefault(right, 0) > 1) {
+//                 char left = s.charAt(i);
+//                 rec.put(left, rec.getOrDefault(left, 0) - 1);
+//                 i++;
+//             }
+            
+//             // 不能count map size因为有些元素value为0，
+//             longest = Math.max(j - i, longest);
+//         }
+        
+//         return longest;
+    
