d253104cd6fdec1148fb3213e6a029d62414b255
DLCoder <lindui1994@gmail.com>
2022-05-31 10:25:59 -0700

    Time: 1 ms (54.66%), Space: 42.6 MB (41.55%) - LeetHub

---
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        maxHeight(root, res);
+        
+        return res;
+    }
+    
+    private int maxHeight(TreeNode root, List<List<Integer>> res) {
+        if (root == null) return 0;
+        
+        int curHeight = Math.max(maxHeight(root.left, res), maxHeight(root.right, res)) + 1;
+        
+        if (res.size() < curHeight) {
+            res.add(new ArrayList<Integer>());
+        }
+        
+        res.get(curHeight - 1).add(root.val);
+        
+        return curHeight;
+    }
+}
