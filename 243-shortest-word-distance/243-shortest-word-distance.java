65ed8c0c52760958cc4ca3841cef5deaf607a479
DLCoder <lindui1994@gmail.com>
2022-06-01 22:36:16 -0700

    Time: 3 ms (35.61%), Space: 44.5 MB (53.50%) - LeetHub

---
+++ 243-shortest-word-distance/243-shortest-word-distance.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int p1 = -1, p2 = -1;
+        int shortest = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i].equals(word1)) {
+                p1 = i;
+            }
+            if (wordsDict[i].equals(word2)) {
+                p2 = i;
+            }
+            
+            if (p1 != -1 && p2 != -1) {
+                shortest = Math.min(Math.abs(p1 - p2), shortest);
+            }
+        }
+        
+        return shortest;
+    }
+}
