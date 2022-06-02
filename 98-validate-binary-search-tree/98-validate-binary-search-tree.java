f4019b48fda43c141059663a1ff52f865462e496
DLCoder <lindui1994@gmail.com>
2022-06-02 13:08:03 -0700

    Time: 0 ms (100.00%), Space: 43.8 MB (54.08%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,6 +14,21 @@
  * }
  */
 class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        
+        boolean left = isValidBST(root.left, minVal, root.val);
+        boolean right = isValidBST(root.right, root.val, maxVal);
+        
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return left && right;
+    }
+    
+    /*
     // inorder traverse
     // 1. 不能单纯的比较左节点小于中间节点，右节点大于中间节点就完事了, 每个node都要满足条件
     // 2. 边界条件 int min-value可能溢出
@@ -38,4 +53,6 @@ class Solution {
         
         return;
     }
+    
+    */
 }
